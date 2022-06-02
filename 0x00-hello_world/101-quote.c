dc861ae22ca1aec006c7627136679ecda6fc5771
Hamso-hub <hamisinasoro44@gmail.com>
2022-06-02 16:18:03 -0700

    betty is a scam

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
 #include <stdio.h>
- /**
-  * main - display using the write function
-  *
-  * Return - set to one
-  */
+
+/**
+ * main - start of main program using puts
+ * Return: set 0 as return value
+ */
+
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
