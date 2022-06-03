e74e85232c67ec805d0d195ecdab9dd279a58c8c
Hamso-hub <hamisinasoro44@gmail.com>
2022-06-03 10:02:00 -0700

    print 1 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print 1 to 9
+ * Return: set to )
+ */
+
+
+int main(void)
+{
+	for (int r = 0; r < 10; r++)
+	{
+		printf("%d", r);
+	}
+	printf("\n");
+	return (0);
+
+
+}
