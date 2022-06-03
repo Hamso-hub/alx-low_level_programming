e193e3a1a8408a8e983146312e1e963fefeda336
Hamso-hub <hamisinasoro44@gmail.com>
2022-06-03 12:33:53 -0700

    base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints single numbers base 10
+ * Return: set to 0
+ */
+
+int main(void)
+
+{
+	int f;
+
+	for (f = 0; f < 10; f++)
+	{
+		putchar(f);
+	}
+	putchar('\n');
+
+	return (0);
+}
