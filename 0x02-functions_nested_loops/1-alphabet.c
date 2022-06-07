7de195f3e560b72f29b764a103a88f64802f7dc7
Hamso-hub <hamisinasoro44@gmail.com>
2022-06-07 14:02:11 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ * Return: set to zero
+ */
+
+int main(void)
+{
+	char xar;
+
+	for (xar = 'a'; xar <= 'z'; xar++)
+	{
+		putchar(xar);
+	}
+
+	putchar('\n');
+	return (0);
+
+}
