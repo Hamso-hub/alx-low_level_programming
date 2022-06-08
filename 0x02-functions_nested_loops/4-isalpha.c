977ec88d0e05fb7a47083010a10cd93493dd7c99
Hamso-hub <hamisinasoro44@gmail.com>
2022-06-08 01:37:21 -0700

    forward

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  _isalpha - checks if c is an alphabet
+ * @c: to be checked
+ * Return: set to 1 if alphabet else 0
+ */
+
+int _isalpha(int c)
+{
+	if (c <= 'a' && c >= 'z' || c <= 'A' && c >= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
