97c80f21bcad2234ea369edbf513981f96dbed37
Hamso-hub <hamisinasoro44@gmail.com>
2022-06-08 02:47:02 -0700

    false

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include 'main.h'
+
+/**
+ * print_last_digit - its the main func
+ * @g: to be evaluated
+ * Return: set fot last digit
+ */
+int print_last_digit(int g)
+
+{
+	if (g % 10 > 0)
+	{
+		return ((g % 10));
+	}
+}
