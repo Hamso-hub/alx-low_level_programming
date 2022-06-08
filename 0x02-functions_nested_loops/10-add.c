065fd4c6238743ecaa28eaaf827ece2efd028609
Hamso-hub <hamisinasoro44@gmail.com>
2022-06-08 03:56:12 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @a: int to be added
+ * @b: int to be added
+ * Return: set to return result
+ */
+
+int add(int a, b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
